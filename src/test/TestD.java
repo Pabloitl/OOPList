c14932883395ed9c64e46ea47f44db93a4f3aa74
RebecaValadez <47796126+RebecaValadez@users.noreply.github.com>
2019-05-16 19:28:41 -0500

    Add files via upload

---
+++ src/test/TestD.java
@@ -0,0 +1,16 @@
+
+package test;
+
+import list.City;
+import list.Display;
+
+public class TestD {
+
+    public static void main(String[] args) {
+        Display d = new Display();
+        
+        d.select();
+        d.city(new City());
+    }
+    
+}
