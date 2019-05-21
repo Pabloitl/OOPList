cd16e5e7882745a4254c2c2adff3d058485a2fb8
RebecaValadez <rebecavaladez67@gmail.com>
2019-05-21 16:59:45 -0500

    .

--- src/list/Display.java
+++ src/list/Display.java
@@ -20,7 +20,7 @@ public class Display {
         JLabel title = new JLabel("Ciudades por visitar");
         JLabel icon  = new JLabel();
         ImageIcon imag = new ImageIcon(new ImageIcon("src/data/toronto.jpg")
-                .getImage().getScaledInstance(480, 380, Image.SCALE_SMOOTH));
+                .getImage().getScaledInstance(480, 320, Image.SCALE_SMOOTH));
         String [] option = {"Añadir ciudad" , "Mostrar ciudad","Editar ciudad"};
         
         UIManager.put("OptionPane.minimumSize", new Dimension(500, 430));
@@ -29,7 +29,7 @@ public class Display {
         title.setBounds(20, 0, 500, 40);
         
         icon.setIcon(imag);
-        icon.setBounds(0, 60, 480, 380);
+        icon.setBounds(0, 45, 480, 320);
         
         panel.setLayout(null);
         panel.add(title);
@@ -57,7 +57,7 @@ public class Display {
         JLabel lPath = new JLabel("Ruta de imagen");
         JLabel icon = new JLabel();
         ImageIcon imag = new ImageIcon(new ImageIcon(
-                "src/data/catedral-san-basilio.jpg")
+                "src/data/paris.jpg")
                 .getImage().getScaledInstance(225, 230, Image.SCALE_SMOOTH));
         
         UIManager.put("OptionPane.minimumSize", new Dimension(600, 250));
@@ -71,7 +71,7 @@ public class Display {
         city.setBounds(310, 10, 150, 20);
         country.setBounds(310, 40, 150, 20);
         continent.setBounds(310, 70, 150, 20);
-        information.setBounds(310, 100, 50, 20);
+        information.setBounds(310, 100, 250, 20);
         path.setBounds(310, 130, 250, 20);
         
         icon.setIcon(imag);
@@ -97,11 +97,11 @@ public class Display {
         }while(!campos(city, country, continent, information,path));
         
         float temperatureAPI = 0;
-        try {
-            temperatureAPI = Weather.askTemp(city.getText());
-        } catch (Exception ex) {
-            temperatureAPI = 0;
-        }
+//        try {
+//            temperatureAPI = Weather.askTemp(city.getText());
+//        } catch (Exception ex) {
+//            temperatureAPI = 0;
+//        }
         
         return new City(city.getText(), country.getText(), continent.getText(), 
                         temperatureAPI, information.getText(), path.getText());
@@ -132,8 +132,7 @@ public class Display {
         JLabel lInformation = new JLabel("Información");
         JLabel lPath = new JLabel("Ruta de imagen");
         JLabel icon = new JLabel();
-        ImageIcon imag = new ImageIcon(new ImageIcon(
-                "src/data/catedral-san-basilio.jpg")
+        ImageIcon imag = new ImageIcon(new ImageIcon("src/data/catedral.jpg")
                 .getImage().getScaledInstance(225, 230, Image.SCALE_SMOOTH));
         
         UIManager.put("OptionPane.minimumSize", new Dimension(600, 250));
@@ -153,7 +152,7 @@ public class Display {
         city.setBounds(310, 10, 150, 20);
         country.setBounds(310, 40, 150, 20);
         continent.setBounds(310, 70, 150, 20);
-        information.setBounds(310, 100, 50, 20);
+        information.setBounds(310, 100, 250, 20);
         path.setBounds(310, 130, 250, 20);
         
         icon.setIcon(imag);
@@ -178,14 +177,14 @@ public class Display {
                                         JOptionPane.DEFAULT_OPTION);
         }while(!campos(city, country, continent, information, path));
         
-        float temperatureAPI;
+        float temperatureAPI = 0;
         
-        try {
-            temperatureAPI = Weather.askTemp(city.getText());
-        } catch (Exception ex) {
-            temperatureAPI = 0;
-        }
-        
+//        try {
+//            temperatureAPI = Weather.askTemp(city.getText());
+//        } catch (Exception ex) {
+//            temperatureAPI = 0;
+//        }
+//        
         return new City(city.getText(), country.getText(), continent.getText(), 
                         temperatureAPI, information.getText(), path.getText());
     }
@@ -215,7 +214,7 @@ public class Display {
         panel.setVisible(true);
         
         return JOptionPane.showInputDialog(null, panel, "Ciudades por visitar", 
-                                    -1, null, args, null).toString();
+                      JOptionPane.DEFAULT_OPTION, null, args, null).toString();
     }
     
     public static void showCity(City c){
@@ -230,21 +229,21 @@ public class Display {
         JLabel eCity  = new JLabel(city);
         JLabel eCountry = new JLabel(c.getCountry().replace("\0", ""));
         JLabel eContinent = new JLabel(c.getContinent().replace("\0", ""));
-        JLabel eTemperature;
-        try{
-            eTemperature = new JLabel(Weather.askTemp(city.replace("\0", "")) + "°");
-        }catch(Exception e){
-            eTemperature = new JLabel(String.valueOf(c.getTemperature()+"°"));
-            showMessage("Error retrieving temperature" + e.getMessage());
-        }
+        JLabel eTemperature = new JLabel("0");
+//        try{
+//            eTemperature = new JLabel(Weather.askTemp(city.replace("\0", "")) + "°");
+//        }catch(Exception e){
+//            eTemperature = new JLabel(String.valueOf(c.getTemperature()+"°"));
+//            showMessage("Error retrieving temperature" + e.getMessage());
+//        }
 
         JLabel eInformation = new JLabel(c.getInformation().replace("\0", ""));
         JLabel icon = new JLabel();
         
         ImageIcon imag = new ImageIcon(new ImageIcon(c.getPath().replace("\0", ""))
-                .getImage().getScaledInstance(400, 390, Image.SCALE_SMOOTH));
+                .getImage().getScaledInstance(400, 250, Image.SCALE_SMOOTH));
         
-        UIManager.put("OptionPane.minimumSize", new Dimension(600, 500));
+        UIManager.put("OptionPane.minimumSize", new Dimension(600, 400));
         
         title.setFont(new Font("Castellar", Font.BOLD, 30));
         title.setBounds(70, 0, 500, 40);
@@ -274,12 +273,12 @@ public class Display {
         eTemperature.setBounds(420, 255, 100, 30);
         
         lInformation.setFont(new Font("AR JULIAN", Font.BOLD, 20));
-        lInformation.setBounds(410, 290, 200, 20);
-        eInformation.setForeground(Color.orange);
-        eInformation.setBounds(420, 312, 100, 30);
+        lInformation.setBounds(10, 305, 200, 20);
+        eInformation.setForeground(Color.blue);
+        eInformation.setBounds(20, 320, 600, 30);
         
         icon.setIcon(imag);
-        icon.setBounds(0, 50, 400, 390);
+        icon.setBounds(0, 50, 400, 250);
         
         panel.setLayout(null);
         panel.add(title);
